a6fec7cef21d5ea337118b43d25e8a4216b57df0
Andrej235 <andrej.nenadic06@gmail.com>
2025-07-02 17:33:21 +0200

    Setup tailwind

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div className="w-full h-full bg-yellow-400">
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    </div>
+  </React.StrictMode>
 );
--- desktop/vite.config.ts
+++ desktop/vite.config.ts
@@ -1,13 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), svgr()],
+  plugins: [react(), tailwindcss(), svgr()],
   clearScreen: false,
   server: {
     port: 1420,
