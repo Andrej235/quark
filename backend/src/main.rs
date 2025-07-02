0f4aebf4a222094a1e621044e3bfde52e13da98e
Banecane109 <dinicbranko007@gmail.com>
2025-07-02 20:29:57 +0200

    Add database registration at server start up

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,43 @@
-use actix_web::{ get, App, HttpResponse, HttpServer, Responder };
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use sea_orm::{Database, DatabaseConnection};
+use std::env;
 
 #[get("/hello")]
-async fn hello() -> impl Responder {
+async fn hello(db: web::Data<DatabaseConnection>) -> impl Responder {
     HttpResponse::Ok().body("Hello World!")
 }
 
+/*
+    This function is used to map endpoints.
+    Any new endpoints that needs to be access has to be registered in this function.
+*/
+fn routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(hello);
+}
+
 #[actix_web::main]
+#[rustfmt::skip]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run().await
+
+    dotenv().ok(); // Makes sure that .env file exists
+
+
+    // Create database connection
+    let database_url: String = env::var("DATABASE_URL").expect("DATABASE_URL not set.");
+    let database_connection: DatabaseConnection = Database::connect(database_url)
+        .await
+        .expect("Failed to connect to database");
+
+
+    // Start actix server
+    println!("Starting server...");
+
+    HttpServer::new(move || {
+        App::new().app_data(web::Data::new(database_connection.clone())) // Inject database into app state
+        .configure(routes) // Register endpoints
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
