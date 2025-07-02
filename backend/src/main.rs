1f79bb0fe30e3406a02149f52e90255d680d100b
Andrej235 <andrej.nenadic06@gmail.com>
2025-07-02 17:25:10 +0200

    Remove prettierrc

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{ get, App, HttpResponse, HttpServer, Responder };
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
@@ -9,6 +9,5 @@ async fn hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(hello))
         .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+        .run().await
 }
