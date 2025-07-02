7d96bdf882ac8d1ee030ed66cad9db8253071e7e
Andrej235 <andrej.nenadic06@gmail.com>
2025-07-02 13:17:10 +0200

    Initialize a tauri app

---
+++ desktop/vite.config.ts
@@ -0,0 +1,27 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [react(), svgr()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
