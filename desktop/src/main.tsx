f7a08ff51863433bbf218c00a507a4e026a8d11e
Andrej235 <andrej.nenadic06@gmail.com>
2025-07-02 17:42:44 +0200

    Setup shadcn

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="h-full w-full bg-yellow-400">
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    <div className="grid size-full min-h-svh place-items-center">
+      <h1 className="text-2xl">Hello World</h1>
     </div>
   </React.StrictMode>,
 );
--- desktop/vite.config.ts
+++ desktop/vite.config.ts
@@ -1,9 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import tailwindcss from "@tailwindcss/vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
@@ -23,6 +23,11 @@ export default defineConfig(async () => ({
       : undefined,
     watch: {
       ignored: ["**/src-tauri/**"],
+    },
+  },
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
