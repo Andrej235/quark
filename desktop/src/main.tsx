ad5dbb3e035d81c965836985c9fc8d835df25bb5
dzinpobede <lukalecic667@gmail.com>
2025-07-03 19:23:04 +0200

    Create first look for login page

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./globals.css";
+import Authentication from "./components/authentication/authentication";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="bg-background grid size-full min-h-svh place-items-center">
-      <h1 className="text-2xl">Hello World</h1>
+    <Authentication />
     </div>
   </React.StrictMode>,
 );
