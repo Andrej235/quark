1b482f04a598d80ea134f947f59f7e8d9e811a05
Banecane109 <dinicbranko007@gmail.com>
2025-07-06 13:00:29 +0200

    Add more info for generated Open API endpoint map

--- backend/src/api_doc.rs
+++ backend/src/api_doc.rs
@@ -1,10 +1,16 @@
 use crate::models::dtos::login_result_dto::LogInResultDTO;
-use crate::routes::auth_routs::{__path_sign_up, __path_log_in, __path_log_out};
 use crate::models::{dtos::create_user_dto::CreateUserDTO, sroute_error::SRouteError};
+use crate::routes::auth_routs::{__path_log_in, __path_log_out, __path_sign_up};
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
 #[openapi(
+    info(
+        title = "Quark Backend Open API",
+        description = "This API handles backend/database operations.",
+        version = "1.0",
+        license(name = "MIT", url = "https://opensource.org/licenses/MIT")
+    ),
     paths(sign_up, log_in, log_out),
     components(schemas(CreateUserDTO, LogInResultDTO, SRouteError)),
     tags(
