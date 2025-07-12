2bf85f0757e2693d9712ba26e970bbcecc5da78f
andjelic-a <23151811a@gmail.com>
2025-07-12 22:38:14 +0000

    Create update team dto

---
+++ backend/src/models/dtos/update_team_dto.rs
@@ -0,0 +1,35 @@
+// ------------------------------------------------------------------------------------
+// IMPORTS
+// ------------------------------------------------------------------------------------
+use crate::traits::endpoint_json_body_data::EndpointJsonBodyData;
+use serde::Deserialize;
+use utoipa::ToSchema;
+use validator::{Validate, ValidationErrors};
+
+// ------------------------------------------------------------------------------------
+// STRUCT
+// ------------------------------------------------------------------------------------
+#[derive(Debug, Clone, ToSchema, Deserialize, Validate)]
+#[rustfmt::skip]
+pub struct CreateTeamDTO {
+
+    #[validate(length(min = 1, max = 150))]
+    pub name:           String,
+
+    pub description:    Option<String>
+}
+
+// ------------------------------------------------------------------------------------
+// IMPLEMENTATIONS
+// ------------------------------------------------------------------------------------
+#[rustfmt::skip]
+impl EndpointJsonBodyData for CreateTeamDTO {
+    fn validate_data(&mut self) -> Result<(), ValidationErrors> {
+
+        // Trim strings
+        self.name = self.name.trim().to_string();
+
+        // Run validation
+        return self.validate();
+    }
+}
