4eb34978cf7bd699d1ab4a03caae65307ca7a5a9
Andrej235 <andrej.nenadic06@gmail.com>
2025-07-16 13:12:17 +0200

    Move the router's root to main.tsx, and add sonner

--- desktop/src/app.tsx
+++ desktop/src/app.tsx
@@ -1,19 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./components/homepage";
-import Login from "./components/login";
-import Signup from "./components/signup";
-import InboxMail from "./components/inbox-mail";
+import { Outlet } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login></Login>} />
-        <Route path="/login" element={<Login></Login>} />
-        <Route path="/signup" element={<Signup></Signup>} />
-        <Route path="/home" element={<Homepage></Homepage>} />
-        <Route path="#inbox" element={<InboxMail></InboxMail>} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-w-svw bg-background min-h-svh">
+      <Outlet />
+
+      <Toaster richColors theme="light" />
+    </div>
   );
 }
