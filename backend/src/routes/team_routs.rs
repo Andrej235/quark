b35b31f2ef8a9c8ef2c60684e71dd98a3aef990c
Banecane109 <dinicbranko007@gmail.com>
2025-07-18 15:27:42 +0200

    When user is deleting team, user is required to have permission for deleting team

--- backend/src/routes/team_routs.rs
+++ backend/src/routes/team_routs.rs
@@ -3,8 +3,13 @@
 // IMPORTS
 //
 // ************************************************************************************
-use crate::entity::team_members::ActiveModel as TeamMemberActiveModel;
-use crate::entity::team_roles::ActiveModel as TeamRoleActiveModel;
+use crate::entity::team_members::{
+    ActiveModel as TeamMemberActiveModel, Column as TeamMemberColumn, Entity as TeamMemberEntity,
+};
+use crate::entity::team_roles::{
+    ActiveModel as TeamRoleActiveModel, Column as TeamRoleColumn, Entity as TeamRoleEntity,
+    Model as TeamRole,
+};
 use crate::entity::teams::{
     ActiveModel as TeamActiveModel, Column as TeamColumn, Entity as TeamEntity,
 };
@@ -28,7 +33,7 @@ use lazy_static::lazy_static;
 use sea_orm::ActiveValue::Set;
 use sea_orm::{
     ActiveModelTrait, ColumnTrait, DatabaseConnection, DbErr, EntityTrait, QueryFilter,
-    TransactionTrait,
+    QuerySelect, TransactionTrait,
 };
 use uuid::Uuid;
 
@@ -182,26 +187,53 @@ pub async fn team_update(
     delete,
     path = TEAM_DELETE_ROUTE_PATH,
     params(
-        ("team_id" = Uuid, Path, description = "ID of the team to delete"),
+        ("team_id" = Uuid, Path, description = "Team ID to delete"),
     ),
     responses(
-        (status = 200, description = "Team delete"),
+        (status = 200, description = "Team deleted"),
+        (status = 403, description = "Possibe messages: Not memeber of team, 
+                                                        Not allowed to delete team", body = SRouteError),
     )
 )]
+#[rustfmt::skip]
 #[delete("/team/delete/{team_id}")]
 pub async fn team_delete(
     db: Data<DatabaseConnection>,
-    _auth_user: BasicAuthenticatedUser,
+    auth_user: AdvancedAuthenticatedUser,
     team_id: Path<Uuid>,
 ) -> impl Responder {
-    // Get ownership of incoming data
-    let id = team_id.into_inner();
+
+    let team_id = team_id.into_inner();
+
+    // Prevent user from deleting team if they are not member of the team or user does not have permission to delete team
+    let team_role: TeamRole = match TeamMemberEntity::find()
+        .filter(TeamMemberColumn::UserId.eq(auth_user.user.id))
+        .filter(TeamMemberColumn::TeamId.eq(team_id))
+        .find_also_related(TeamRoleEntity)
+        .one(db.get_ref())
+        .await {
+        Ok(Some((_, Some(team_role)))) => team_role,
+        Ok(Some((_, None))) | Ok(None) => {
+            return HttpResponse::Forbidden().json(SRouteError { message: "Not memeber of team" });
+        },
+        Err(err) => {
+            return endpoint_internal_server_error(
+                TEAM_DELETE_ROUTE_PATH,
+                "Finding team member",
+                Box::new(err),
+            );
+        }
+    };
+
+    if Role::from_bits_truncate(team_role.permissions).contains(Role::CAN_DELETE_TEAM) == false {
+        return HttpResponse::Forbidden().json(SRouteError { message: "Not allowed to delete team" });
+    }
 
     // Delete team
-    let delete_result = TeamEntity::delete_by_id(id).exec(db.get_ref()).await;
+    let delete_result = TeamEntity::delete_by_id(team_id).exec(db.get_ref()).await;
 
     match delete_result {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {},
         Err(err) => {
             return endpoint_internal_server_error(
                 TEAM_DELETE_ROUTE_PATH,
@@ -210,4 +242,6 @@ pub async fn team_delete(
             );
         }
     }
+
+    return HttpResponse::Ok().finish();
 }
