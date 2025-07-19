bbbbe66b0b5a188b42e9ffa9fb993f78532892af
Andrej235 <andrej.nenadic06@gmail.com>
2025-07-19 10:26:39 +0200

    Add sendApiRequest's options to use query wrapper

--- desktop/src/api-dsl/use-query.ts
+++ desktop/src/api-dsl/use-query.ts
@@ -5,7 +5,10 @@ import {
 } from "@tanstack/react-query";
 import sendApiRequest from "./send-api-request";
 import { Endpoints, Methods } from "./types/endpoints/endpoints";
-import { Parameters, Request } from "./types/endpoints/request-parser";
+import {
+  Parameters as ApiParameters,
+  Request,
+} from "./types/endpoints/request-parser";
 import { UnwrappedApiResponse } from "./types/endpoints/response-parser";
 
 type GetRoutes = keyof {
@@ -16,7 +19,8 @@ type Options<Route extends GetRoutes> = OmitKeyof<
   UseQueryOptions<unknown, unknown, unknown, unknown[]>,
   "initialData" | "queryFn"
 > &
-  Parameters<Route, "get">;
+  ApiParameters<Route, "get"> &
+  Parameters<typeof sendApiRequest>[2];
 
 export default function useQuery<Route extends GetRoutes>(
   route: Route,
@@ -24,11 +28,23 @@ export default function useQuery<Route extends GetRoutes>(
 ): ReturnType<typeof useTanQuery<UnwrappedApiResponse<Route, "get">>> {
   return useTanQuery({
     queryFn: () =>
-      sendApiRequest(route, {
-        method: "get",
-        parameters:
-          options && "parameters" in options ? options.parameters : undefined,
-      } as unknown as Request<Route>).then((x) => {
+      sendApiRequest(
+        route,
+        {
+          method: "get",
+          parameters:
+            options && "parameters" in options ? options.parameters : undefined,
+        } as unknown as Request<Route>,
+        {
+          abortSignal: options?.abortSignal,
+          omitCredentials: options?.omitCredentials,
+          showToast: options?.showToast as true,
+          toastOptions:
+            options && "toastOptions" in options
+              ? options.toastOptions
+              : undefined,
+        },
+      ).then((x) => {
         if (x?.isOk) return x.response;
 
         throw new Error(x?.error?.message ?? "Something went wrong");
