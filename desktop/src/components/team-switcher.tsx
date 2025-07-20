b518fb4e3c1b005e3f7e58c8b79470df0c8f4226
Andrej235 <andrej.nenadic06@gmail.com>
2025-07-20 11:05:00 +0200

    Fix team switcher failing to open on click and add a link to new team page

--- desktop/src/components/team-switcher.tsx
+++ desktop/src/components/team-switcher.tsx
@@ -15,17 +15,21 @@ import {
 } from "@/components/ui/sidebar";
 import { useUserStore } from "@/stores/user-store";
 import { ChevronsUpDown, LucideUsers2, Plus } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { Link } from "react-router-dom";
 
 export function TeamSwitcher() {
   const { isMobile } = useSidebar();
   const user = useUserStore((x) => x.user);
-  const teams = user?.teamsName ?? [];
-  const defaultTeam = user?.defaultTeamId ?? teams[0]?.id;
 
-  const [activeTeam, setActiveTeam] = useState(
-    teams.find((x) => x.id === defaultTeam),
+  const teams = useMemo(() => user?.teamsName ?? [], [user?.teamsName]);
+  const defaultTeam = useMemo(
+    () => teams.find((x) => x.id === (user?.defaultTeamId ?? teams[0]?.id)),
+    [teams, user?.defaultTeamId],
   );
+
+  const [activeTeam, setActiveTeam] = useState<typeof defaultTeam | null>(null);
+  useEffect(() => setActiveTeam(defaultTeam ?? null), [defaultTeam]);
 
   // This will be falsy if the user is not logged in, still loading, or has no teams
   if (!activeTeam) {
@@ -36,21 +40,24 @@ export function TeamSwitcher() {
     <SidebarMenu>
       <SidebarMenuItem>
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
+          <SidebarMenuButton
+            size="lg"
+            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            asChild
+          >
+            <DropdownMenuTrigger>
               <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                 <LucideUsers2 className="size-4" />
               </div>
+
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{activeTeam.name}</span>
                 <span className="truncate text-xs">Role</span>
               </div>
+
               <ChevronsUpDown className="ml-auto" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
+            </DropdownMenuTrigger>
+          </SidebarMenuButton>
           <DropdownMenuContent
             className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
             align="start"
@@ -74,11 +81,16 @@ export function TeamSwitcher() {
               </DropdownMenuItem>
             ))}
             <DropdownMenuSeparator />
-            <DropdownMenuItem className="gap-2 p-2">
-              <div className="flex size-6 items-center justify-center rounded-md border bg-transparent">
-                <Plus className="size-4" />
-              </div>
-              <div className="text-muted-foreground font-medium">Add team</div>
+            <DropdownMenuItem className="gap-2 p-2" asChild>
+              <Link to="/new-team">
+                <div className="flex size-6 items-center justify-center rounded-md border bg-transparent">
+                  <Plus className="size-4" />
+                </div>
+
+                <div className="text-muted-foreground font-medium">
+                  Add team
+                </div>
+              </Link>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
