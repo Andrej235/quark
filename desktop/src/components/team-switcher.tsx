c9c3660fc2b6bf8881542a77c1c634616d61888d
Andrej235 <andrej.nenadic06@gmail.com>
2025-07-20 11:57:05 +0200

    Create a custom context menu for each team in team switcher which allows users to set a team as their new default

--- desktop/src/components/team-switcher.tsx
+++ desktop/src/components/team-switcher.tsx
@@ -1,3 +1,4 @@
+import sendApiRequest from "@/api-dsl/send-api-request";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -13,14 +14,32 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
 import { useUserStore } from "@/stores/user-store";
-import { ChevronsUpDown, LucideUsers2, Plus } from "lucide-react";
+import {
+  ChevronsUpDown,
+  LogOut,
+  LucideCheckCircle,
+  LucideUsers2,
+  Plus,
+  Star,
+} from "lucide-react";
 import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuTrigger,
+} from "./ui/context-menu";
+import { Schema } from "@/api-dsl/types/endpoints/schema-parser";
+import { toast } from "sonner";
 
 export function TeamSwitcher() {
   const { isMobile } = useSidebar();
   const user = useUserStore((x) => x.user);
+  const setUser = useUserStore((x) => x.setUser);
 
   const teams = useMemo(() => user?.teamsName ?? [], [user?.teamsName]);
   const defaultTeam = useMemo(
@@ -30,6 +49,41 @@ export function TeamSwitcher() {
 
   const [activeTeam, setActiveTeam] = useState<typeof defaultTeam | null>(null);
   useEffect(() => setActiveTeam(defaultTeam ?? null), [defaultTeam]);
+
+  async function handleSetSetDefault(team: Schema<"TeamInfoDTO">) {
+    if (team.id === user?.defaultTeamId) {
+      toast.info("Team already set as default");
+      return;
+    }
+
+    const { isOk } = await sendApiRequest(
+      "/user/me/default-team/{team_id}",
+      {
+        method: "patch",
+        parameters: {
+          team_id: team.id,
+        },
+      },
+      {
+        showToast: true,
+        toastOptions: {
+          loading: "Setting default team, please wait...",
+          success: `Successfully set ${team.name} as default team!`,
+        },
+      },
+    );
+
+    if (!isOk) return;
+
+    setUser({ ...user!, defaultTeamId: team.id });
+  }
+
+  function handleLeaveTeam(team: Schema<"TeamInfoDTO">) {
+    setUser({
+      ...user!,
+      teamsName: user!.teamsName.filter((x) => x.id !== team.id),
+    });
+  }
 
   // This will be falsy if the user is not logged in, still loading, or has no teams
   if (!activeTeam) {
@@ -67,20 +121,62 @@ export function TeamSwitcher() {
             <DropdownMenuLabel className="text-muted-foreground text-xs">
               Teams
             </DropdownMenuLabel>
+
             {teams.map((team, index) => (
-              <DropdownMenuItem
-                key={team.name}
-                onClick={() => setActiveTeam(team)}
-                className="gap-2 p-2"
-              >
-                <div className="flex size-6 items-center justify-center rounded-md border">
-                  <LucideUsers2 className="size-3.5 shrink-0" />
-                </div>
-                {team.name}
-                <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
-              </DropdownMenuItem>
+              <ContextMenu key={team.name}>
+                <ContextMenuTrigger asChild>
+                  <DropdownMenuItem
+                    onClick={() => setActiveTeam(team)}
+                    className="gap-2 p-2"
+                  >
+                    <div className="flex size-6 items-center justify-center rounded-md border">
+                      <LucideUsers2 className="size-3.5 shrink-0" />
+                    </div>
+                    {team.name}
+                    <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </ContextMenuTrigger>
+
+                <ContextMenuContent className="w-64">
+                  <ContextMenuItem onClick={() => setActiveTeam(team)}>
+                    <span>Select</span>
+
+                    <LucideCheckCircle
+                      className={cn(
+                        "ml-auto",
+                        activeTeam.id === team.id && "stroke-green-300",
+                      )}
+                    />
+                  </ContextMenuItem>
+
+                  <ContextMenuItem onClick={() => handleSetSetDefault(team)}>
+                    <span>Set as default</span>
+
+                    <Star
+                      className={cn(
+                        "ml-auto",
+                        user?.defaultTeamId === team.id &&
+                          "fill-yellow-300 stroke-yellow-300",
+                      )}
+                    />
+                  </ContextMenuItem>
+
+                  <ContextMenuSeparator />
+
+                  <ContextMenuItem
+                    className="group"
+                    onClick={() => handleLeaveTeam(team)}
+                  >
+                    <span>Leave team</span>
+
+                    <LogOut className="group-hover:stroke-destructive ml-auto" />
+                  </ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
             ))}
+
             <DropdownMenuSeparator />
+
             <DropdownMenuItem className="gap-2 p-2" asChild>
               <Link to="/new-team">
                 <div className="flex size-6 items-center justify-center rounded-md border bg-transparent">
