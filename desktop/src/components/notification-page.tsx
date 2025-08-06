c2f4cc7a05d06fecbee7c42923b4421cf8d638d7
dzinpobede <lukalecic667@gmail.com>
2025-08-06 12:28:29 +0200

    Add Notification item function on notification page

--- desktop/src/components/notification-page.tsx
+++ desktop/src/components/notification-page.tsx
@@ -10,7 +10,7 @@ import {
 
 export default function NotificationPage() {
   return (
-    <div className="flex w-full items-center justify-center gap-4 flex-col">
+    <div className="flex w-full flex-col items-center justify-center gap-4">
       <div className="mt-4 flex w-full items-center justify-center gap-4">
         <div className="flex flex-col">
           <Select defaultValue={"inbox"}>
@@ -41,13 +41,37 @@ export default function NotificationPage() {
         </div>
       </div>
 
-      <div className="w-full h-full ">
-        <div className="bg-secondary w-full h-full min-h-full rounded-sm grid ">
-          <div className="flex gap-2 items-center m-4">
+      <div className="h-full w-full">
+        <div className="bg-secondary grid h-full min-h-full w-full rounded-sm">
+          <div className="flex h-full w-full items-center gap-2 rounded-sm border-2 p-4">
             <Checkbox className="bg-input"></Checkbox>
             <h1>Select all</h1>
           </div>
+
+          <div className="bt-0 flex h-full w-full flex-col gap-4 rounded-sm border-2 border-t-0 p-4">
+            <NotificationItem />
+            <NotificationItem />
+            <NotificationItem />
+            <NotificationItem />
+          </div>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function NotificationItem() {
+  return (
+    <div className="flex justify-between">
+      <div className="flex items-center gap-4">
+        <Checkbox className="bg-input"></Checkbox>
+        <div className="flex flex-col">
+          <h1>Notification title or user</h1>
+          <p>Notification description</p>
+        </div>
+      </div>
+      <div>
+        <p>Time</p>
       </div>
     </div>
   );
