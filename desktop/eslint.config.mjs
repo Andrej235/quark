386699357405f116c8a3000c1d0889fbf2b559fe
Andrej235 <andrej.nenadic06@gmail.com>
2025-08-16 18:36:05 +0200

    Disable eslint no-redeclare error which prevents overloads from being declared

--- desktop/eslint.config.mjs
+++ desktop/eslint.config.mjs
@@ -47,10 +47,7 @@ export default [
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-redeclare": [
-        "error",
-        { ignoreDeclarationMerge: true },
-      ],
+      "no-redeclare": "off",
     },
     settings: {
       react: {
